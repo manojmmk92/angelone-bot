345ff452402e0dcc56ad66c30f76d3611202a32f
manojmmk92 <manojmmk@hotmail.com>
2025-06-22 13:00:58 +0530

    Fix import for SmartConnect

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
-from smartapi import SmartConnect
+from flask import Flask, request,
+from smartapi.smartConnect import SmartConnect
 import pyotp
 import os
 
